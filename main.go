1afb28b66b5a4271b411e8c71db507e2e68bb8ce
fupeng <fupenglxy@gmail.com>
2023-11-29 23:24:00 +0800

    feat: live reload

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	"gopkg.in/fsnotify.v1"
@@ -13,16 +14,15 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: go run main.go <path-to-your-program>")
+		fmt.Println("Usage: golive <path-to-your-program>")
 		os.Exit(1)
 	}
 
 	programPath, _ := filepath.Abs(os.Args[1])
+	programArgs := os.Args[2:]
 
-	// Run the initial version of the program
-	restart(programPath)
+	restart(programPath, programArgs...)
 
-	// Watch for file changes
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal("Error creating watcher:", err)
@@ -34,14 +34,18 @@ func main() {
 		log.Fatal("Error adding file to watcher:", err)
 	}
 
-	// Watch for changes in a separate goroutine
-	go watchForChanges(watcher, programPath)
+	go watchForChanges(watcher, func() {
+		clearConsole()
+		restart(programPath, programArgs...)
+	})
 
 	// Wait for termination signal
 	select {}
 }
 
-func watchForChanges(watcher *fsnotify.Watcher, programPath string) {
+var debouncer *time.Timer
+
+func watchForChanges(watcher *fsnotify.Watcher, f func()) {
 	for {
 		select {
 		case event := <-watcher.Events:
@@ -50,31 +54,39 @@ func watchForChanges(watcher *fsnotify.Watcher, programPath string) {
 				continue
 			}
 
-			fmt.Printf("File %s changed. Restarting...\n", event.Name)
+			log.Printf("File %s changed. Restarting...\n", event.Name)
 
-			// Delay the restart to avoid issues with editor saving
-			time.Sleep(500 * time.Millisecond)
+			if debouncer != nil {
+				debouncer.Stop()
+			}
 
-			// Restart the program on file change
-			restart(programPath)
-
+			// Debounce restart
+			debouncer = time.AfterFunc(500*time.Millisecond, f)
 		case err := <-watcher.Errors:
 			log.Println("Error watching file:", err)
 		}
 	}
 }
 
-func restart(programPath string) {
+var cmd *exec.Cmd
+
+func restart(programPath string, args ...string) {
+	running := cmd != nil && cmd.Process != nil
+
 	// Stop the previous process if it's running
-	if cmd != nil && cmd.Process != nil {
+	if running {
 		err := cmd.Process.Kill()
 		if err != nil {
 			log.Println("Error killing previous process:", err)
 		}
 	}
 
-	// Start a new process
-	cmd = exec.Command("go", "run", programPath)
+	var execArgs []string
+
+	execArgs = append(execArgs, "run", programPath)
+	execArgs = append(execArgs, args...)
+
+	cmd = exec.Command("go", execArgs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
@@ -84,7 +96,21 @@ func restart(programPath string) {
 		return
 	}
 
-	fmt.Println("Process restarted")
+	if running {
+		log.Println("Process restarted")
+	}
 }
 
-var cmd *exec.Cmd
+func clearConsole() {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	default:
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
